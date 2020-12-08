97b85a216f13dca329197cb6eecc65aaa3ab5d02
indexytesla <65568449+indexytesla@users.noreply.github.com>
2020-12-08 18:18:36 +0530

    Update on-complete.sh

--- on-complete.sh
+++ on-complete.sh
@@ -10,7 +10,7 @@ INFO="[${LIGHT_GREEN_FONT_PREFIX}INFO${FONT_COLOR_SUFFIX}]"
 
 echo -e "$(date +"%m/%d %H:%M:%S") ${INFO} Delete .aria2 file ..."
 
-if [ "$2" -eq 0 ]; then
+if [ $2 == 0 ]; then
     exit 0
 elif [ -e "${filepath}.aria2" ]; then
     rm -vf "${filepath}.aria2"
@@ -20,7 +20,7 @@ fi
 echo -e "$(date +"%m/%d %H:%M:%S") ${INFO} Delete .aria2 file finish"
 echo "$(($(cat numUpload)+1))" > numUpload # Plus 1
 
-if [[ "$2" -eq 1 ]]; then # single file
+if [[ $2 == 1 ]]; then # single file
 	rclone -v --config="rclone.conf" copy "$3" "DRIVE:$RCLONE_DESTINATION" 2>&1	
 elif [[ $2 -gt 1 ]]; then # multiple file
 	rclone -v --config="rclone.conf" copy "$topPath" "DRIVE:$RCLONE_DESTINATION/${relativePath%%/*}"
